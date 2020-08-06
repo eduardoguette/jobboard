bb04a5e7668989b66f1300e09cbc0ea683ad9639
Eduardo Guette <eduardoguette@github.com>
2020-08-06 13:00:54 +0200

    done

--- src/static/Components/Header/Header.js
+++ src/static/Components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Header.css";
-import imageheader from "../../images/bg-header-mobile.svg";
 
 export default function Header() {
   return (
     <>
       <header>
-       
+
       </header>
     </>
   );
